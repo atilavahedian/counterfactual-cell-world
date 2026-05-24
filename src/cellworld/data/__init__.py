3504fa1ac021f2cc3a60bf97cef4776011b73568
Atila Vahedian <atila1387.vahedian@gmail.com>
2026-05-24 15:47:42 -0700

    Expose CELLxGENE discovery helper

--- src/cellworld/data/__init__.py
+++ src/cellworld/data/__init__.py
@@ -2,12 +2,14 @@
 
 from cellworld.data.synthetic import SyntheticWorldConfig, build_synthetic_splits
 from cellworld.data.types import CellCondition, CellPopulationDataset, collate_conditions
+from cellworld.data.cellxgene import CellxGeneCollection, list_cellxgene_collections
 
 __all__ = [
     "CellCondition",
     "CellPopulationDataset",
+    "CellxGeneCollection",
     "SyntheticWorldConfig",
     "build_synthetic_splits",
     "collate_conditions",
+    "list_cellxgene_collections",
 ]
-
